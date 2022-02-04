dc269dc68372287eb9457a5deb5c2f03d16a296b
SalifAbdoulSow18 <abdoulsalifsow06@gmail.com>
2022-02-04 17:54:11 +0000

    gestion de la partie coup de coeur

--- src/components/Card.js
+++ src/components/Card.js
@@ -73,17 +73,29 @@ const Card = ({ movie }) => {
       }
         return genreArray.map((genre) => <li key={genre}>{genre}</li>)
     };
-    
+
+    const addStorage = () =>{
+        let storedData = window.localStorage.movies
+            ? window.localStorage.movies.split(",")
+            : [];
+        if (!storedData.includes(movie.id.toString())) {
+            storedData.push(movie.id);
+            window.localStorage.movies = storedData;
+        }
+    }
     return (
         <div className="card">
             <img src={movie.poster_path ? "https://image.tmdb.org/t/p/w500" + movie.poster_path : "./img/poster.jpg"} alt="afficher film"/>
             <h2>{movie.title}</h2>
             { movie.release_date ? (<h5>sortie le: {dataFormater(movie.release_date)}</h5>) : ("") }
             <h4> {movie.vote_average}/10 <span>⭐</span> </h4>
-            <ul>{genreFinder()}</ul>
+            <ul>{ movie.genre_ids
+                ? genreFinder()
+                : movie.genres.map((genre, index) => <li key={index}>{genre.name}</li>)}
+            </ul>
             { movie.overview ? (<h3>Synopsis</h3>) : ""}
             <p>{movie.overview}</p>
-            <div className="btn">Ajouter aux coups de coeur</div>
+            <div className="btn" onClick={() => addStorage()}>Ajouter aux coups de coeur</div>
         </div>
     );
 };
--- src/pages/UserList.js
+++ src/pages/UserList.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Header from '../components/Header';
+import axios from "axios";
+import Card from "../components/Card";
 
 const UserList = () => {
+    const [listData, setListData] = useState([]);
+    useEffect(() => {
+        const moviesId = window.localStorage.movies
+            ? window.localStorage.movies.split(",")
+            : [];
+        for (let i = 0; i < moviesId.length; i++ ) {
+            axios.get(`https://api.themoviedb.org/3/movie/${moviesId[i]}?api_key=abb9450bd063c3a1bc49399618f32935&language=fr-FR`)
+                .then((res) => setListData((listData) => [...listData, res.data]))
+        }
+    },[])
     return (
-        <div>
+        <div className="user-list-page">
             <Header />
+            <h2>Coup de coeur <span>💖</span></h2>
+            <div className="result">
+                {listData.length > 0
+                    ? (listData.map((movie) => <Card movie={movie} key={movie.id} />))
+                    : (
+                        <h2>Aucun coup de coeur pour le moment</h2>
+                    )
+                }
+            </div>
         </div>
     );
 };
